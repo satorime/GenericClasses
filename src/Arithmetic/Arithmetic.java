ea4722d6d8b7b67385ec6105bbd709e6bccb8f3a
Capstone <brix.bitayo@gmail.com>
2024-03-30 13:50:41 +0800

    Initial commit

--- src/Arithmetic/Arithmetic.java
+++ src/Arithmetic/Arithmetic.java
@@ -1,2 +1,44 @@
-public class Arithmetic {
+package Arithmetic;
+
+public class Arithmetic<B1, B2> {
+    private B1 num1;
+    private B2 num2;
+
+    public Arithmetic(B1 num1, B2 num2) {
+        if (!(num1 instanceof Number) || !(num2 instanceof Number)) {
+            throw new IllegalArgumentException("Invalid Types!");
+        }
+
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public Number add() {
+        return ((Number) num1).doubleValue() + ((Number) num2).doubleValue();
+    }
+
+    public Number subtract() {
+        return ((Number) num1).doubleValue() - ((Number) num2).doubleValue();
+    }
+
+    public Number multiply() {
+        return ((Number) num1).doubleValue() * ((Number) num2).doubleValue();
+    }
+
+    public Number divide() {
+        Integer divisor = ((Number) num2).intValue();
+        if (divisor == 0) {
+            throw new ArithmeticException("Division by zero not allowed!");
+        }
+
+        return ((Number) num1).doubleValue() / ((Number) num2).doubleValue();
+    }
+
+    public Number getMin() {
+        return Math.min(((Number) num1).doubleValue(), ((Number) num2).doubleValue());
+    }
+
+    public Number getMax() {
+        return Math.max(((Number) num1).doubleValue(), ((Number) num2).doubleValue());
+    }
 }
--- src/Map/MyMap.java
+++ src/Map/MyMap.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class MyMap {
+package Map;
+
+import java.util.ArrayList;
+
+public class MyMap<B1, B2> {
+    private ArrayList<B1> keys;
+    private ArrayList<B2> val;
+
+    public MyMap() {
+        keys = new ArrayList<>();
+        val = new ArrayList<>();
+    }
+
+    public void put(B1 key, B2 value) {
+        int index = keys.indexOf(key);
+        if (index == -1) {
+            keys.add(key);
+            val.add(value);
+        } else {
+            val.set(index, value);
+        }
+    }
+
+    public B2 get(B2 key) {
+        int index = keys.indexOf(key);
+        if (index == -1) {
+            return null;
+        } else {
+            return val.get(index);
+        }
+    }
+
+    public B2 remove(B1 key) {
+        int index = keys.indexOf(key);
+        if (index == -1) {
+            return null;
+        } else {
+            keys.remove(index);
+            return val.remove(index);
+        }
+    }
 }
